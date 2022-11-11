0fa0b1101f7aac79958a6d9190471f6cd9c548ab
GT.YANG <gaga0099@gmail.com>
2022-11-11 16:34:09 +0800

    update

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import time
 
-print("程式啟點")
+print("程式起點")
 st.title('這是我的第一個streamlit標題')
 st.header('這是我的次標題')
 st.subheader('這是我的三級標題')
@@ -39,6 +40,21 @@ with st.sidebar:
     ---
     這是***段落2-2***
     ''')
-    st.button('按鈕1')
+    btn1 = st.button('按鈕1')
+    if btn1:
+        print("btn1被按了")
+
+def downloaddata():
+    print("下載資料")
+
+# 要給一個跳出迴圈的指令
+# while True:
+#     time.sleep(5)
+#     print('過了五秒')
+#     downloaddata()
+    # st.experimental_rerun
+
+# while True:
+
 
 print('程式結束點')
