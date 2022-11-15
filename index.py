df5e8d46639c524f0b814f43001aaa29edba79a8
GT.YANG <gaga0099@gmail.com>
2022-11-15 10:40:02 +0800

    加入firebase套件，準備做實時數據庫

--- index.py
+++ index.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
 
+# Application Default credentials are automatically created.
+cred = credentials.Certificate('private/raspberry1-58efc-firebase-adminsdk-tzk5o-6836a56c1e.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 st.title('光線和距離即時監控')
 
 def downloaddata():
     print("下載資料")
-
+    
